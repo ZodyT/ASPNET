a845e72d3accc83e67b75ea3f966568ccb30c1fe
Zody <Zoraida1988@yahoo.com>
2021-04-09 20:36:19 -0500

    ProductRepository

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+using Dapper;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        IEnumerable<Product> IProductRepository.GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * from PRODUCTS;");
+        }
+    }
+}
